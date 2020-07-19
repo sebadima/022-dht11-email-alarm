87dd83fc7f33e51c52b87dd5f8c04833cd27d26e
sebastiano <me@sebadima.com>
2020-07-19 21:11:08 +0200

    fix 011

--- credentials_sample.h
+++ credentials_sample.h
@@ -1,5 +1,5 @@
 #define WIFI_SSID          "Wifi ssid";
 #define WIFI_PASSWD        "Wifi password";
-#define EMAIL_MITTENTE     "Your mail account es: (name@gmail.com)";
-#define PASSWORD_EMAIL     "Youe email password";
+#define EMAIL_MITTENTE     "Your mail account es: (yourname@gmail.com)";
+#define PASSWORD_EMAIL     "Your email password";
 #define EMAIL_DESTINATARIO "Target email address";
