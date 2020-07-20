aad819f9013f1b1373bd220fce22d7b497f21a7c
sebastiano <me@sebadima.com>
2020-07-20 08:52:54 +0200

    modif ita credentials_sample

--- credentials_sample.h
+++ credentials_sample.h
@@ -1,5 +1,5 @@
-#define WIFI_SSID          "Wifi ssid";
-#define WIFI_PASSWD        "Wifi password";
-#define EMAIL_MITTENTE     "Your mail account es: (yourname@gmail.com)";
-#define PASSWORD_EMAIL     "Your email password";
-#define EMAIL_DESTINATARIO "Target email address";
+#define WIFI_SSID          "Nome della rete Wifi";
+#define WIFI_PASSWD        "Password della rete wifi";
+#define EMAIL_MITTENTE     "La mail che usi per inviare es: (nome@gmail.com)";
+#define PASSWORD_EMAIL     "La password del tuo account email";
+#define EMAIL_DESTINATARIO "La email cui arrivano gli avvisi";
